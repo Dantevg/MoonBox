0bff7a5f0aa08638b714b2dba3b2404360248743
Dantevg <dantevg006@gmail.com>
2019-11-22 17:26:37 +0100

    Add argument type checking, re-add net API

--- env/net.lua
+++ env/net.lua
@@ -1,31 +1,55 @@
 --[[
 	
 	Net API
-	Provides internet access
-	
-	(currently disabled)
+	Provides network access
 	
 ]]--
+
+local http = require "socket.http"
 
 local net = {}
+local args = {...}
+local computer = args[1]
+local love = args[2]
 
---[[
-local http = require "socket.http"
-local ltn12 = require "ltn12"
+
+
+-- FUNCTIONS
 
 function net.request(url)
-	local response = {}
-	local body, code, headers, status = http.request{
-		url = url,
-		sink = ltn12.sink.table(response)
-	}
+	expect( url, "string" )
+	
+	local body, code, headers, status = http.request(url)
 	
 	return {
-		body = table.concat(response),
-		code = code
+		body = body,
+		code = code,
+		status = status
 	}
 end
-]]--
+
+function net.udpInit()
+	net.udp = socket.udp()
+	net.udp:setsockname( "*", 64242 )
+	net.udp:settimeout(0)
+end
+
+function net.udpSend( data, ip, port )
+	expect( data, "string", 1, "net.udpSend" )
+	expect( ip, "string", 2, "net.udpSend" )
+	expect( port, "number", 3, "net.udpSend" )
+	
+	if not net.udp then net.udpInit() end
+	net.udp:setpeername( ip, port )
+	local success, err = net.udp:send(data)
+	return success == 1, err
+end
+
+function net.udpReceive()
+	if not net.udp then net.udpInit() end
+	
+	return net.udp:receivefrom()
+end
 
 
 
