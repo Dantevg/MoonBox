db8c7f9ad582188ad4f1f665bf3d082ef6dd444c
Dantevg <dantevg006@gmail.com>
2019-11-22 17:25:46 +0100

    Add argument type checking, add thread functions

--- env/os.lua
+++ env/os.lua
@@ -1,15 +1,16 @@
 --[[
 	
 	OS API
-	Provides OS functions for time/timing, shutdown/reboot communication and execution
+	Provides OS functions for time/timing,
+		shutdown/reboot communication, execution and threads
 	
 ]]--
 
+local osOld = os
 local os = {}
 local args = {...}
 local computer = args[1]
 local love = args[2]
-local osOld = args[3].os
 
 
 
@@ -27,6 +28,9 @@ function os.clock()
 end
 
 function os.time( h24, seconds )
+	expect( h24, {"boolean", "nil"}, 1, "os.time" )
+	expect( seconds, {"number", "nil"}, 2, "os.time" )
+	
 	if h24 then
 		return osOld.date( "%H:%M"..(seconds and ":%S" or "") )
 	else
@@ -35,6 +39,8 @@ function os.time( h24, seconds )
 end
 
 function os.date(yearFirst)
+	expect( yearFirst, {"boolean", "nil"} )
+	
 	if yearFirst then
 		return osOld.date("%Y-%m-%d")
 	else
@@ -47,6 +53,8 @@ function os.datetime()
 end
 
 function os.startTimer(time)
+	expect( time, {"number","nil"} )
+	
 	time = math.ceil( (time or 0)*computer.FPS ) / computer.FPS
 	time = math.max( time, 1/computer.FPS )
 	table.insert( computer.timers, computer.timers.n, love.timer.getTime() + time )
@@ -55,6 +63,8 @@ function os.startTimer(time)
 end
 
 function os.sleep(time)
+	expect( time, {"number", "nil"} )
+	
 	local timer = os.startTimer(time)
 	repeat
 		local event, param = event.wait("timer")
@@ -78,6 +88,8 @@ end
 -- EXECUTION FUNCTIONS
 
 function os.run( path, ... )
+	expect( path, "string", 1, "os.run" )
+	
 	if not disk.exists(path) then
 		error( "No such file", 2 )
 	end
@@ -97,10 +109,115 @@ function os.run( path, ... )
 end
 
 function os.elevate(fn)
+	expect( fn, "function" )
+	
 	event.wait("elevateRequest")
 	coroutine.yield(fn)
 	local result = {event.wait("elevateResult")}
 	return unpack( result, 2 )
+end
+
+
+
+-- THREAD FUNCTIONS
+
+local thread = {}
+
+-- os.newThread( source (string) [,immediate (boolean)] )
+-- source: path to file, or string of code when immediate == true
+function os.newThread( source, immediate )
+	expect( source, "string", 1, "os.newThread" )
+	expect( immediate, {"boolean", "nil"}, 2, "os.newThread" )
+	
+	if immediate then
+		source = source.."\n" -- For love.thread.newThread() to recognize as a code string
+	elseif not disk.exists(source) then
+		error( "No such file", 2 )
+	end
+	
+	return setmetatable(
+		{
+			source = immediate and source or disk.read(source),
+			thread = love.thread.newThread("/rom/programs/thread.lua"),
+			channel = love.thread.newChannel()
+		},
+		{__index = function(t,k)
+			if k == "running" then
+				return t.thread:isRunning()
+			elseif k == "error" then
+				return t.thread:getError()
+			elseif thread[k] then
+				return thread[k]
+			else
+				return rawget(t,k)
+			end
+		end}
+	)
+end
+
+function thread:start(...)
+	self.thread:start( self.channel, self.source, ... )
+end
+
+function thread:wait()
+	self.thread:wait()
+end
+
+-- Thread:send( data (any) [,wait (number|boolean)] )
+-- wait:	- true: wait until message accepted
+-- 				-    0: don't wait
+-- 				-   >0: wait until message accepted or until timeout reached
+function thread:send( data, wait )
+	expect( wait, {"tonumber", "nil"}, 2, "(Thread):send" )
+	
+	if not data then return end
+	wait = tonumber(wait) or 0
+	
+	if wait == true then -- Wait until message accepted
+		return self.channel:supply(data)
+	elseif wait > 0 then -- Wait until message accepted or specific timeout
+		return self.channel:supply( data, wait )
+	else -- Don't wait
+		self.channel:push(value)
+		return true
+	end
+end
+
+-- Thread:receive( [type (string)] )
+-- type:	- "peek" (don't remove message from queue)
+-- 				- "wait" (wait until message is available)
+function thread:receive(type)
+	expect( type, {"string", "nil"} )
+	
+	if type == "peek" then
+		return self.channel:peek()
+	elseif type == "wait" then
+		return self.channel:demand()
+	else
+		return self.channel:pop()
+	end
+end
+
+function os.newWindow()
+	return setmetatable(
+		{
+			thread = love.thread.newThread(
+				'os.execute("C:/program files/LOVE/love.exe" '..love.filesystem.getSource()..')\n'
+			),
+			channel = love.thread.newChannel()
+		},
+		{__index = function(t,k)
+			if k == "active" then
+				return self.thread:isRunning()
+			elseif k == "error" then
+				return self.thread:getError()
+			elseif thread[k] then
+				return thread[k]
+			else
+				return rawget(t,k)
+			end
+		end}
+	)
 end
 
 --[[os.blacklists = {}
