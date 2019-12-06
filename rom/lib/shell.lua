6c84966ccdf878075a4e5fd0b7760b895df6006c
Dantevg <mail@dantevg.nl>
2019-12-06 18:15:00 +0100

    Change autocompletion function

--- rom/lib/shell.lua
+++ rom/lib/shell.lua
@@ -73,19 +73,19 @@ end
 function shell.autocomplete(input)
 	local path = disk.getPath( shell.absolute(input) )
 	local search = disk.getFilename( shell.absolute(input) )
+	if string.sub( input, -1 ) == "/" then -- Peek into dir
+		path = path.."/"..search
+		search = ""
+	end
+	
 	if not disk.exists(path) or disk.info(path).type == "file" then
 		return false
 	end
 	
 	local files = disk.list(path)
 	for k, file in ipairs(files) do
-		local filePath = path.."/"..file
-		if disk.info(path.."/"..file).type ~= "file" then
-			filePath = filePath.."/"
-		end
-		local s, e = string.find( filePath, "/"..search, 1, true )
-		if s then
-			return string.sub( filePath, (string.sub(input, -1) == "/" and s+1 or s) + #search + 1 )
+		if string.sub( file, 1, #search ) == search then
+			return string.sub( file, #search+1, -1 )
 		end
 	end
 end
