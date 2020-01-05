6f0cd2967ec0b52eebc40eea258b8354504fd635
Dantevg <mail@dantevg.nl>
2020-01-05 13:26:44 +0100

    Uptimize

--- rom/programs/hexview.lua
+++ rom/programs/hexview.lua
@@ -18,11 +18,9 @@ end
 local start, length = 1, screen.charHeight * width
 local maxScroll = math.ceil( (#file-length)/width ) * width + 1
 
--- Main loop
-while running do
-	screen.clear()
-	local x, y = 1, 1
-	for i = start, math.min( start + length - 1, #file ) do
+local function writeLine( start, y )
+	local x = 1
+	for i = start, math.min( start + width - 1, #file ) do
 		local char = string.sub(file,i,i)
 		local xHex = ((x-1)*3 + math.floor((x-1)/4)) * (screen.font.width+1) + 1
 		local xChar = ((x-1)+width*3 + math.floor(width/4)) * (screen.font.width+1) + 1
@@ -34,25 +32,40 @@ while running do
 		else
 			screen.write( ".", {x = xChar, y = yScreen, color="gray-1"} )
 		end
-		
 		x = x+1
-		if i % width == 0 then
-			x = 1
-			y = y+1
-		end
 	end
-	
+end
+
+local function redraw()
+	screen.clear()
+	local y = 1
+	for i = start, start+length, width do
+		writeLine( i, y )
+		y = y+1
+	end
+end
+
+redraw()
+
+-- Main loop
+while running do
 	local _, key = event.wait("key")
 	if key == "q" or key == "escape" then
 		running = false
-	elseif key == "up" then
+	elseif key == "up" and start > width then
 		start = math.max( start - width, 1 )
-	elseif key == "down" then
+		screen.move( 0, screen.font.height+1 )
+		writeLine( start, 1 )
+	elseif key == "down" and start < maxScroll then
 		start = math.min( start + width, math.max(1, maxScroll) )
+		screen.move( 0, -screen.font.height-1 )
+		writeLine( start + length - width, screen.charHeight )
 	elseif key == "pageup" then
 		start = math.max( start - length, 1 )
+		redraw()
 	elseif key == "pagedown" then
 		start = math.min( start + length, math.max(1, maxScroll) )
+		redraw()
 	end
 end
 
