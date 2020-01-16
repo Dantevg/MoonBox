f877fd6574ced19153208762c2b12419fdb2cda1
Dantevg <mail@dantevg.nl>
2020-01-16 16:27:46 +0100

    Add swizzle masking lib

---
+++ rom/lib/swizzle.lua
@@ -0,0 +1,157 @@
+--[[
+	
+	Swizze masking lib
+	Provides swizzle masking functionality like in GLSL
+	(eg. colour[1] == colour.r, pos[2] == pos.y)
+	
+	* This lib only provides swizzle *masking*, not the swizzling itself (like pos1 = pos2.yx)
+	
+	I tried to make this lib as adaptive as possible, combining all coding styles
+	to make everybody happy :)
+	- swizzle.new and swizzle.set can both be called with a table as data
+		or with individual arguments as data
+	- swizzle() is alias for swizzle.new()
+	- swizzled tables have their own .set() function, but you can also use the general :set()
+	
+	Usage:
+	- These all do exactly the same:
+		swizzle.new(...), swizzle.new({...}), swizzle.new{...},
+		swizzle(...), swizzle({...}), swizzle{...}
+	- To replace a swizzled table: (these also do exactly the same)
+		swizzle.set( t, ... ), swizzle.set( t, {...} ),
+		t:set(...), t:set({...}), t:set{...},
+		t.set(...), t.set({...}), t.set{...}
+	
+]]--
+
+local swizzle = {}
+
+swizzle.map = {
+	r = 1, g = 2, b = 3, a = 4, -- RGBA colour
+	h = 1, s = 2, l = 3, a = 4, -- HSLA colour
+	x = 1, y = 2, z = 3,				-- XYZ coordinates
+	w = 1, h = 2								-- W/H sizes
+}
+
+
+
+-- HELPER FUNCTIONS
+
+local function mtFn( a, b, fn )
+	if type(a) == "number" then
+		a, b = b, a
+	end
+	
+	local t = {}
+	if type(b) == "table" then
+		if #a ~= #b then error( "Tables must be of same length", 3 ) end
+		for i = 1, #a do
+			table.insert( t, fn(a[i], b[i]) )
+		end
+	else
+		for i = 1, #a do
+			table.insert( t, fn(a[i], b) )
+		end
+	end
+	
+	return setmetatable( t, swizzle.mt )
+end
+
+
+
+-- METATABLE FUNCTIONS
+
+swizzle.mt = {}
+swizzle.mt.swizzle = true
+
+function swizzle.mt.__index( t, k )
+	if swizzle.map[k] and t[ swizzle.map[k] ] then
+		return t[ swizzle.map[k] ]
+	end
+	if k == "set" then
+		return swizzle.set
+	end
+end
+
+function swizzle.mt.__newindex( t, k, v )
+	if swizzle.map[k] then
+		t[ swizzle.map[k] ] = v
+	end
+end
+
+function swizzle.mt.__add( a, b )
+	return mtFn( a, b, function(a,b) return a+b end )
+end
+
+function swizzle.mt.__sub( a, b )
+	return mtFn( a, b, function(a,b) return a-b end )
+end
+
+function swizzle.mt.__mul( a, b )
+	return mtFn( a, b, function(a,b) return a*b end )
+end
+
+function swizzle.mt.__div( a, b )
+	return mtFn( a, b, function(a,b) return a/b end )
+end
+
+function swizzle.mt.__mod( a, b )
+	return mtFn( a, b, function(a,b) return a%b end )
+end
+
+function swizzle.mt.__pow( a, b )
+	return mtFn( a, b, function(a,b) return a^b end )
+end
+
+function swizzle.mt.__unm(a)
+	return mtFn( a, nil, function(a) return -a end )
+end
+
+function swizzle.mt.__eq( a, b )
+	if #a ~= #b then return false end
+	for i = 1, #a do
+		if a[i] ~= b[i] then return false end
+	end
+	return true
+end
+
+function swizzle.mt.__tostring(t)
+	return "("..table.concat( t, "," )..")"
+end
+
+
+
+-- FUNCTIONS
+
+function swizzle.new(...)
+	local arg = {...}
+	if #arg > 1 or type( arg[1] ) ~= "table" then
+		arg.set = function(...) return swizzle.set(arg,...) end
+		return setmetatable( arg, swizzle.mt )
+	else
+		arg[1].set = function(...) return swizzle.set(arg,...) end
+		return setmetatable( arg[1], swizzle.mt )
+	end
+end
+
+function swizzle.set( t, ... )
+	local arg = {...}
+	local data = arg[1]
+	if #arg > 1 or type( arg[1] ) ~= "table" then
+		data = arg
+	end
+	
+	for i = 1, math.max( #data, #t ) do
+		t[i] = data[i]
+	end
+end
+
+
+
+-- RETURN
+
+return setmetatable( swizzle, {
+	__call = function(_,...)
+		return swizzle.new(...)
+	end
+} )
