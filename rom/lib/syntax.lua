6e8ac071f3d56ec0dc556581dd631fe023ba9556
Dantevg <mail@dantevg.nl>
2020-02-13 18:39:00 +0100

    Add ordered tablle of patterns, update patterns (I didn't know lua patterns that well as I thought I did)

--- rom/lib/syntax.lua
+++ rom/lib/syntax.lua
@@ -9,7 +9,7 @@ local syntax = {}
 syntax.patterns = {}
 
 local block = "%[(=*)%[.-%]%1%]"
-local stringContent = "([^\n]*[\\\n]?)-[^\\]"
+local stringContent = "[^\n]-[^\\]"
 
 syntax.patterns.comment = {
 	"^%-%-[^\n]*",            -- Single-line comment
@@ -19,27 +19,38 @@ syntax.patterns.comment = {
 syntax.patterns.string = {
 	'^"'..stringContent..'"', -- Single-line comment with double quotes("")
 	"^'"..stringContent.."'", -- Single-line comment with single quotes ('')
-	block,                    -- Multiline comment
+	block,                    -- Multiline string
 }
 
 syntax.patterns.number = {"^%d+"}
 
 syntax.patterns.punctuation = {"^%p+"}
 
-syntax.patterns.word = {"^[%a_][%w_]*"}
-
 syntax.patterns.keyword = {
-	"and", "break", "do", "else", "elseif",
-	"end", "false", "for", "function", "if",
-	"in", "local", "nil", "not", "or",
-	"repeat", "return", "then", "true", "until", "while"
+	"^and", "^break", "^do", "^else", "^elseif",
+	"^end", "^false", "^for", "^function", "^if",
+	"^in", "^local", "^nil", "^not", "^or",
+	"^repeat", "^return", "^then", "^true", "^until", "^while"
 }
+
+syntax.patterns.word = {"^[%a_][%w_]*"}
 
 syntax.patterns.whitespace = {"^%s+"}
 
+syntax.patternsOrder = {
+	"comment",
+	"string",
+	"number",
+	"punctuation",
+	"keyword",
+	"word",
+	"whitespace",
+}
+
 function syntax.match( s, from )
 	from = from or 1
-	for type, patterns in pairs(syntax.patterns) do
+	for _, type in ipairs(syntax.patternsOrder) do
+		local patterns = syntax.patterns[type]
 		for i, pattern in ipairs(patterns) do
 			local match = string.match( s, pattern, from )
 			if match then return match, type, from, from + #match end
@@ -50,7 +61,8 @@ end
 function syntax.matchAll( s, from )
 	from = from or 1
 	local matches = {}
-	for type, patterns in pairs(syntax.patterns) do
+	for _, type in ipairs(syntax.patternsOrder) do
+		local patterns = syntax.patterns[type]
 		for i, pattern in ipairs(patterns) do
 			local match = string.match( s, pattern, from )
 			table.insert( matches, {
@@ -69,7 +81,7 @@ function syntax.gmatch(s)
 	return function()
 		local str, type, from, to = syntax.match( s, start )
 		if not str then return end
-		start = start + to
+		start = start + to - from
 		return str, type, from, to
 	end
 end
