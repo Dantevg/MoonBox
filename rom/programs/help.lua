3efb3946f10561f1b0c8e320c0f65552f64a7e36
Dantevg <mail@dantevg.nl>
2020-03-01 16:36:18 +0100

    Add help functionality

--- rom/programs/help.lua
+++ rom/programs/help.lua
@@ -1,1 +1,13 @@
-print("Help coming soon...")
+local what = ...
+if what and disk.exists("/docs/"..what..".md") then
+	os.run( "/rom/programs/view.lua", "/docs/"..what..".md" )
+else
+	print( "Topics:", "green+1" )
+	local files = disk.list("/docs")
+	for i = 1, #files do
+		local path = "/docs/"..files[i]
+		if disk.info(path).type == "file" and disk.getFilename(path) ~= "README.md" then
+			print( "- "..string.sub( files[i], 1, -#disk.getExtension(path)-1 ) )
+		end
+	end
+end
